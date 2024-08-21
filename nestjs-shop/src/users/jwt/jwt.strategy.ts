00ad84d691257b7895d5a2ae2359c6c6124e7839
JHeok <skdhd999@gmail.com>
2024-08-21 16:56:14 +0900

    jwt

--- nestjs-shop/src/users/jwt/jwt.strategy.ts
+++ nestjs-shop/src/users/jwt/jwt.strategy.ts
@@ -4,11 +4,11 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),  // 헤더로부터 토큰 추출하는 함수
-      secretOrKey: "key1234",
-      ignoreExpiration: false,
-    });
-  }
+    constructor() {
+      super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),  // 헤더로부터 토큰 추출하는 함수
+        secretOrKey: "key1234",
+        ignoreExpiration: false,
+      });
+    }
 }

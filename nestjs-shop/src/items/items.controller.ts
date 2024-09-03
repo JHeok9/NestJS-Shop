14fe19cca44d9dee240a6ade85ad69ead3d112f2
JHeok <skdhd999@gmail.com>
2024-09-03 11:58:38 +0900

    상품추가 기능

--- nestjs-shop/src/items/items.controller.ts
+++ nestjs-shop/src/items/items.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { JwtAuthGuard } from 'src/users/jwt/jwt.guard';
+import { CreateItemDto } from './dto/item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    constructor(private itemsService: ItemsService){};
+
+
+    // 상품 추가
+    @UseGuards(JwtAuthGuard)
+    @Post('create')
+    async createItem(@Req() req, @Body() createItemDto: CreateItemDto) {
+        createItemDto.userid = req.user.userid;
+        return this.itemsService.createItem(createItemDto);
+    }
+
+}
--- nestjs-shop/src/items/items.service.ts
+++ nestjs-shop/src/items/items.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Items } from './entity/items.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateItemDto } from './dto/item.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    constructor(
+        @InjectRepository(Items) private itemsRepository: Repository<Items>,
+    ) {}
+
+    async createItem(createItemDto: CreateItemDto) {
+        await this.itemsRepository.save(createItemDto);
+
+    }
+
+}
